9826b323338be821560b3ec32cd9a7d6f48d7a5b
sanketwakhare <sanketwakhare@gmail.com>
2023-02-25 19:54:04 +0530

    change topic and group_id

--- src/main/java/com/sanket/bloggingplatformcounterservice/common/AppConstants.java
+++ src/main/java/com/sanket/bloggingplatformcounterservice/common/AppConstants.java
@@ -2,6 +2,6 @@ package com.sanket.bloggingplatformcounterservice.common;
 
 public class AppConstants
 {
-	public static final String TOPIC_NAME = "test";
-	public static final String GROUP_ID = "group_id";
+	public static final String TOPIC_NAME = "blogging";
+	public static final String GROUP_ID = "counter_service_group";
 }
